272ed7afde41295534d381dead3a33f776c39f93
matheus <engmatheusmantini@gmail.com>
2022-10-20 08:40:28 -0300

    Add products controller test

---
+++ src/products/tests/__mocks__/index.ts
@@ -0,0 +1,14 @@
+import { ProductsEntity } from "src/products/entity/products.entity";
+
+export const productsEntityList: ProductsEntity[] = [
+  { id: '1', name: 'Product 1', price: 1.11, qty_stock: 111 },
+  { id: '2', name: 'Product 2', price: 2.22, qty_stock: 222 },
+  { id: '3', name: 'Product 3', price: 3.33, qty_stock: 333 },
+];
+
+export const updatedproductsEntity = {
+  id: '1',
+  name: 'Product 1',
+  price: 1.11,
+  qty_stock: 110,
+};
